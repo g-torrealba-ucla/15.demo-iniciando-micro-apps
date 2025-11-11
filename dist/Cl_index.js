ff7b26ad9a6d63617dda90d74905671dadd3eaac
g-torrealba-ucla <g_torrealba@ucla.edu>
2025-11-10 21:28:24 -0400

    251110-2125: ajuste menor

--- dist/Cl_index.js
+++ dist/Cl_index.js
@@ -5,42 +5,22 @@
  * Debe permitir buscar por operadora telefónica (Movistar, Digitel, Movilnet).
  * Debe permitir buscar por número agenda de teléfono.
  */
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
+import Cl_controlador from "./Cl_controlador.js";
 import Cl_mAgenda from "./Cl_mAgenda.js";
-import { dtContactos } from "./data/dtContactos.js";
+import Cl_vAgenda from "./Cl_vAgenda.js";
 export default class Cl_index {
     constructor() {
-        this.modelo = new Cl_mAgenda();
-        this.cargarInicialDatos();
-        // modelo.cargar((error: string | false) => {
-        //   if (error) alert(error);
-        //   if (error) throw new Error(error);
-        //   let vista = new Cl_vAgenda();
-        //   let controlador = new Cl_controlador(modelo, vista);
-        //   vista.controlador = controlador;
-        //   vista.refresh();
-        // });
-    }
-    cargarInicialDatos() {
-        return __awaiter(this, void 0, void 0, function* () {
-            for (const contacto of dtContactos) {
-                yield new Promise((res) => setTimeout(res, 250)); // Espera 50ms
-                this.modelo.agregarContacto({
-                    contactoData: contacto,
-                    callback: (error) => {
-                        if (error)
-                            console.error(error);
-                    },
-                });
-            }
+        let modelo = new Cl_mAgenda();
+        // this.cargarInicialDatos();
+        modelo.cargar((error) => {
+            if (error)
+                alert(error);
+            if (error)
+                throw new Error(error);
+            let vista = new Cl_vAgenda();
+            let controlador = new Cl_controlador(modelo, vista);
+            vista.controlador = controlador;
+            vista.refresh();
         });
     }
 }
--- src/Cl_index.ts
+++ src/Cl_index.ts
@@ -12,28 +12,27 @@ import Cl_vAgenda from "./Cl_vAgenda.js";
 import { dtContactos } from "./data/dtContactos.js";
 
 export default class Cl_index {
-  public modelo: Cl_mAgenda;
   constructor() {
-    this.modelo = new Cl_mAgenda();
-    this.cargarInicialDatos();
-    // modelo.cargar((error: string | false) => {
-    //   if (error) alert(error);
-    //   if (error) throw new Error(error);
-    //   let vista = new Cl_vAgenda();
-    //   let controlador = new Cl_controlador(modelo, vista);
-    //   vista.controlador = controlador;
-    //   vista.refresh();
-    // });
+    let modelo = new Cl_mAgenda();
+    // this.cargarInicialDatos();
+    modelo.cargar((error: string | false) => {
+      if (error) alert(error);
+      if (error) throw new Error(error);
+      let vista = new Cl_vAgenda();
+      let controlador = new Cl_controlador(modelo, vista);
+      vista.controlador = controlador;
+      vista.refresh();
+    });
   }
-  async cargarInicialDatos() {
-    for (const contacto of dtContactos) {
-      await new Promise((res) => setTimeout(res, 250)); // Espera 50ms
-      this.modelo.agregarContacto({
-        contactoData: contacto,
-        callback: (error: string | false) => {
-          if (error) console.error(error);
-        },
-      });
-    }
-  }
+  // async cargarInicialDatos() {
+  //   for (const contacto of dtContactos) {
+  //     await new Promise((res) => setTimeout(res, 250)); // Espera 50ms
+  //     this.modelo.agregarContacto({
+  //       contactoData: contacto,
+  //       callback: (error: string | false) => {
+  //         if (error) console.error(error);
+  //       },
+  //     });
+  //   }
+  // }
 }
